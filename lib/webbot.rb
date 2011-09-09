d31d01e5944a6a638ac335d86c053bc27f3c1fd2
Diogo Kamioka <farofitus@gmail.com>
2011-09-09 19:11:38 -0300

    acessando uma pagina

--- lib/webbot.rb
+++ lib/webbot.rb
@@ -1,5 +1,10 @@
+require 'open-uri'
 MAILRXP = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/) 
 
 def getmail(webpage)
   webpage.scan(MAILRXP).uniq
+end
+
+def pagina(url)
+  a = URI.parse(url).read.to_s
 end
