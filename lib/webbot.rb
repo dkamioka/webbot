05199bd038390f43fbccf9e98f49da7c5b5f707e
Diogo Kamioka <farofitus@gmail.com>
2011-09-12 19:14:03 -0300

    usando agora tree, sera?

--- lib/webbot.rb
+++ lib/webbot.rb
@@ -1,10 +1,21 @@
 require 'open-uri'
 MAILRXP = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/) 
 
-def getmail(webpage)
-  webpage.scan(MAILRXP).uniq
+module webbot
+  class url_tree
+    attr_accessor :node
+
+    def initialize()
+      @node = Tree::TreeNode.new("Root",0,"")
+    end
+
+    def getmail()
+      @node[2].scan(MAILRXP).uniq
+    end
+    
+    def pagina(url)
+      a = URI.parse(url).read.to_s
+    end
+  end
 end
 
-def pagina(url)
-  a = URI.parse(url).read.to_s
-end
